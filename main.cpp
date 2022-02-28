47313aa39669419d1ea5743db7b7e05411255a4c
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-28 20:55:04 +0100

    added examples of rvalue reference

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main() {
     bool b = false; //rvalue
     std::cout << "adres str: " << &"hello there" << '\n'; //lvalue
 
+
+    /////////C++98
+
     std::string s = "s"; //lvalue = lvalue
     std::string &ref_s = s; //ref lvalue = lvalue
 
@@ -16,6 +19,15 @@ int main() {
     const std::string &const_ref_s3 = s; //const ref
     //const_ref_s3 += "dfs"; //nie można modyfikowac
     const std::string &const_ref_s4 = "dflhdsifjs"; //const ref = rvalue
+
+
+    //////////////C++11
+    std::string s5 = "fgsdfg";
+
+    //rvalue reference
+    //std::string &&rv_ref_s5 = s5; //nie mozna przypisac lvalue
+    std::string &&rv_ref_s6 = "dfgdgsdgs"; //mozna przypisac obiekt tymczasowy
+    rv_ref_s6 += "fgdrgdfg"; //mozna modyfikowac
 
 
 }
