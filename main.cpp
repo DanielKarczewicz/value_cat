458eaba85aacd8e870d13768cf1bf41de7571ec0
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-02-28 20:11:32 +0100

    added examples of lvalue and rvalue cat C++98

--- main.cpp
+++ main.cpp
@@ -6,4 +6,16 @@ int main() {
     bool b = false; //rvalue
     std::cout << "adres str: " << &"hello there" << '\n'; //lvalue
 
+    std::string s = "s"; //lvalue = lvalue
+    std::string &ref_s = s; //ref lvalue = lvalue
+
+    //nie można zainicjować odwołania typu
+    //"std::string &" (bez kwalifikatora const) przy użyciu wartości typu "const char [2]"
+    //std::string &ref_s2 = "h";
+    
+    const std::string &const_ref_s3 = s; //const ref
+    //const_ref_s3 += "dfs"; //nie można modyfikowac
+    const std::string &const_ref_s4 = "dflhdsifjs"; //const ref = rvalue
+
+
 }
