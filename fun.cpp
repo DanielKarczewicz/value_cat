a2f9a73e1b414f98b27edebd02d2a35f5226ff33
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-03-01 11:34:09 +0100

    added comments to overloaded functions

--- fun.cpp
+++ fun.cpp
@@ -2,20 +2,21 @@
 
 void fun(int&&) {std::cout << "rvalue ref int&&\n";}
 /*
-1. 
+1. rvalue ref (5)
 
 
 */
 void fun(int&) {std::cout << "lvalue ref int&\n";} 
 /*
-1.
+1. lvalue (int i = 10)
 
 
 */
 void fun(const int&) {std::cout << "const ref const int&\n";} 
 /*
-1.
-
+1. const lvalue ref (const int &x = 10)
+2. rvalue ref (5)
+3. lvalue (int i = 10)
 
 */
 
@@ -25,5 +26,9 @@ int main() {
     const int &x = 10; //const lvalue ref
     fun(i); //lvalue
     fun(x); //const lvalue ref
-    fun(546); //rvalue ref
+    fun(5); //rvalue ref
+
+    //int i = 10; - pasuje do int& i const int&
+    //const int &x = 10; - pasuje do const int&
+    //5 - pasuje do const int&
 }
