17a1d8d5e30a7848ba3ef0c506cea9694ffa8116
DanielKarczewicz <dkarczewicz.2000@gmail.com>
2022-03-02 18:07:06 +0100

    added functions

---
+++ vec.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> addOne(std::vector<int> vec) {
+    vec.push_back(1);
+    return vec;
+}
+
+void addOneOpt(std::vector<int>& vec) {
+    vec.push_back(1);
+} //przekazujemy przez adres
+
+int addOne(int intCheapToCopy) {
+    intCheapToCopy += 1;
+    return intCheapToCopy;
+}
+
+int main() {
+
+    int i = 10;
+    int iOne = addOne(i);
+
+
+    std::vector<int> vec(100, 6);
+    addOneOpt(vec);
+
+    std::cout << vec.size() << '\n';
+
+}
